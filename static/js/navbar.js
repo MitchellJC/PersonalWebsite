1485b9e7835312e4ccff45ea134e2559fe4605c0
jubilee67 <81349046+jubilee67@users.noreply.github.com>
2021-07-08 13:01:38 +1000

    Finished responsive navbar.
    Navbar works for screens starting at a width of 320px.

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -1,6 +1,7 @@
 const mobileMenuButton = document.getElementById("mobile-menu")
 	nav = document.querySelector("nav")
-	mobileExitMenuButton = document.getElementById("mobile-menu-exit");
+	mobileExitMenuButton = document.getElementById("mobile-menu-exit")
+	externalNavMobile = document.getElementById("external-nav-mobile")
 
 mobileMenuButton.addEventListener("click", () => {
 	nav.classList.add("mobile-menu-open")
