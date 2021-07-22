73cdf1a40010131c7fe4c3c37c30cc5b213d0462
jubilee67 <81349046+jubilee67@users.noreply.github.com>
2021-07-22 21:18:47 +1000

    Started adding animations to homepage.

--- static/js/homepage.js
+++ static/js/homepage.js
@@ -1,10 +1,26 @@
 const heroButtons = document.querySelectorAll(".hero-button") 
-	projectsTitle = document.getElementById("projects-title")
+gsap.registerPlugin(ScrollTrigger)
+gsap.registerPlugin(ScrollToPlugin);
 
 for (var i = 0; i < heroButtons.length; i++){
 	let heroButton = heroButtons[i]
 
 	heroButton.addEventListener("click", () => {
-	projectsTitle.scrollIntoView()
+		gsap.to(window, {duration: 1, scrollTo:"#projects-title", ease: "elastic"});
 	})
 }
+
+var tl = gsap.timeline()
+
+tl.from("#hero-container", {
+	y: "-30%",
+	opacity: 0,
+	duration: 1
+})
+
+tl.from(".stagger1", {
+	y: "-30%",
+	opacity: 0,
+	stagger: 0.3,
+	duration: 0.5
+}, "-=0.5")
