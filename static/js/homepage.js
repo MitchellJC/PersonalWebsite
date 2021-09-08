a4cc2e5a6598fd82dfccd73ac4fd8998b4224aee
jubilee67 <81349046+jubilee67@users.noreply.github.com>
2021-09-08 22:25:33 +1000

    Fixed personal website image.

--- static/js/homepage.js
+++ static/js/homepage.js
@@ -1,28 +1,28 @@
-const heroButtons = document.querySelectorAll(".hero-button") 
+const heroButtons = document.querySelectorAll(".hero-button"); 
 gsap.registerPlugin(ScrollTrigger);
 gsap.registerPlugin(ScrollToPlugin);
-var tl = gsap.timeline()
+var tl = gsap.timeline();
 
 for (var i = 0; i < heroButtons.length; i++){
-	let heroButton = heroButtons[i]
+	let heroButton = heroButtons[i];
 
 	heroButton.addEventListener("click", () => {
 		gsap.to(window, {duration: 0.000001, scrollTo: {y: "#projects-title", offsetY: 50}});
-	})
+	});
 };
 
 tl.from("#hero-container", {
 	y: "-30%",
 	opacity: 0,
 	duration: 1
-})
+});
 
 tl.from(".stagger1", {
 	y: "-30%",
 	opacity: 0,
 	stagger: 0.3,
 	duration: 0.5
-}, "-=0.5")
+}, "-=0.5");
 
 gsap.from("#projects-title", {
 	scrollTrigger: {
@@ -32,7 +32,7 @@ gsap.from("#projects-title", {
 
 	x: "-30%",
 	opacity: 0
-})
+});
 
 gsap.from(".project", {
 	scrollTrigger: {
@@ -43,4 +43,4 @@ gsap.from(".project", {
 	y: "30%",
 	opacity: 0,
 	stagger: 0.3
-})
+});
--- static/js/navbar.js
+++ static/js/navbar.js
@@ -1,12 +1,12 @@
 const mobileMenuButton = document.getElementById("mobile-menu")
 	nav = document.querySelector("nav")
 	mobileExitMenuButton = document.getElementById("mobile-menu-exit")
-	externalNavMobile = document.getElementById("external-nav-mobile")
+	externalNavMobile = document.getElementById("external-nav-mobile");
 
 mobileMenuButton.addEventListener("click", () => {
 	nav.classList.add("mobile-menu-open")
-})
+});
 
 mobileExitMenuButton.addEventListener("click", () => {
 	nav.classList.remove("mobile-menu-open")
-})
+});
