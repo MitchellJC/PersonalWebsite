1c70f424ccce1cf03414e09bd1a83f785080ef61
jubilee67 <81349046+MitchellJC@users.noreply.github.com>
2022-06-28 21:29:22 +1000

    getelementsbytagname not working for courses

--- static/js/about.js
+++ static/js/about.js
@@ -1,3 +1,5 @@
+
+
 var tl = gsap.timeline();
 
 function init() {
@@ -7,6 +9,26 @@ function init() {
 		duration: 1,
 		stagger: 0.3
 	});
+
+	(function(){
+		console.log("HELOO")
+		const courses = document.getElementsByTagName("course");
+		console.log(courses)
+		// Connect each course to its description                     
+		for (const course of courses) {
+			const title = course.getElementsByTagName("course-title")[0];
+			const description = course.getElementsByClassName("course-description-box")[0];
+	
+			title.addEventListener("click", () => {
+				console.log("Clicked");
+				if (description.classList.contains("course-box-open")) {
+					description.classList.remove("course-box-open");
+				} else {
+					description.classList.add("course-box-open");
+				}
+			});
+		}
+	})();
 }
 
 window.addEventListener("load", function(event){
